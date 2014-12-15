f847498e9ea911cd94629a875ba9da6caec60a1e
Kaitlin Devine <kaitlin.devine@gsa.gov>
2014-12-15 17:21:42 -0500

    updated data loader

--- contracts/management/commands/load_data.py
+++ contracts/management/commands/load_data.py
@@ -18,8 +18,8 @@ class Command(BaseCommand):
                 if line[0]:
                     #create contract record, unique to vendor, labor cat
                     idv_piid = line[0]
-                    vendor_name = line[3]
-                    labor_category = line[4].strip().replace('\n', ' ')
+                    vendor_name = line[7]
+                    labor_category = line[8].strip().replace('\n', ' ')
                     
                     try:
                         contract = Contract.objects.get(idv_piid=idv_piid, labor_category=labor_category, vendor_name=vendor_name)
@@ -30,29 +30,33 @@ class Command(BaseCommand):
                         contract.labor_category = labor_category
                         contract.vendor_name = vendor_name
 
-                    contract.education_level = contract.get_education_code(line[5])
-                    if line[1] != '':
-                        contract.contract_start = datetime.strptime(line[1], '%m/%d/%Y').date()
-                    if line[2] != '':
-                        contract.contract_end = datetime.strptime(line[2], '%m/%d/%Y').date()
+                    contract.education_level = contract.get_education_code(line[9])
+                    contract.schedule = line[2]
+                    contract.business_size = line[1]
+                    contract.sin = line[6]
+
+                    if line[4] != '':
+                        contract.contract_start = datetime.strptime(line[4], '%m/%d/%Y').date()
+                    if line[5] != '':
+                        contract.contract_end = datetime.strptime(line[5], '%m/%d/%Y').date()
                 
-                    if line[6].strip() != '':
-                        contract.min_years_experience = line[6]
+                    if line[10].strip() != '':
+                        contract.min_years_experience = line[10]
                     else:
                         contract.min_years_experience = 0
 
-                    if line[7] and line[7] != '': 
-                        contract.hourly_rate_year1 = contract.normalize_rate(line[7])
+                    if line[11] and line[11] != '': 
+                        contract.hourly_rate_year1 = contract.normalize_rate(line[11])
                     else:
                         #there's no pricing info
                         continue
                     
-                    for count, rate in enumerate(line[8:12]):
+                    for count, rate in enumerate(line[12:]):
                         if rate and rate.strip() != '':
                             setattr(contract, 'hourly_rate_year' + str(count+2), contract.normalize_rate(rate))
                     
                     
-                    contract.contractor_site = line[12]
+                    contract.contractor_site = line[3]
 
                     contract.save()
             except Exception as e:
