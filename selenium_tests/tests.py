d3f8cd948ebe7736190c38762879023a213c4d8c
Shawn Allen <shawn.allen@gsa.gov>
2015-04-01 14:10:41 -0700

    add monkey patch to quell connection errors

--- selenium_tests/tests.py
+++ selenium_tests/tests.py
@@ -391,6 +391,38 @@ def find_column_header(driver, col_name):
 def get_column_headers(driver):
     return driver.find_elements_by_xpath('//thead/tr/th')
 
+# We only need this monkey patch here because the stack traces clutter up the
+# test results output. --shawn
+def patch_broken_pipe_error():
+    """
+    Monkey patch BaseServer.handle_error to not write a stack trace to stderr
+    on broken pipe: <http://stackoverflow.com/a/22618740/362702>
+    """
+    import sys
+    from SocketServer import BaseServer
+    from wsgiref import handlers
+
+    handle_error = BaseServer.handle_error
+    log_exception = handlers.BaseHandler.log_exception
+
+    def is_broken_pipe_error():
+        type, err, tb = sys.exc_info()
+        r = repr(err)
+        return r in ("error(32, 'Broken pipe')", "error(54, 'Connection reset by peer')")
+
+    def my_handle_error(self, request, client_address):
+        if not is_broken_pipe_error():
+            handle_error(self, request, client_address)
+
+    def my_log_exception(self, exc_info):
+        if not is_broken_pipe_error():
+            log_exception(self, exc_info)
+
+    BaseServer.handle_error = my_handle_error
+    handlers.BaseHandler.log_exception = my_log_exception
+
+patch_broken_pipe_error()
+
 
 
 if __name__ == '__main__':
