8754732bae85fe2b9f58b4fba523d8686566c83e
Shawn Allen <shawn.allen@gsa.gov>
2015-04-29 13:51:26 -0700

    try postgres user

--- hourglass/local_settings.travis.py
+++ hourglass/local_settings.travis.py
@@ -4,7 +4,7 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'hourglass',
-        'USER': 'hourglass',
+        'USER': 'postgres',
         'PASSWORD': '',
     }
 }
