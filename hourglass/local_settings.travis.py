8f0759f52a382f85cc6f716dbb55a723813bd58c
Shawn Allen <shawn.allen@gsa.gov>
2015-04-29 12:49:06 -0700

    database setup, travis-specific local_settings.py

---
+++ hourglass/local_settings.travis.py
@@ -0,0 +1,25 @@
+DEBUG = True
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'hourglass',
+        'USER': 'hourglass',
+        'PASSWORD': '',
+    }
+}
+
+SECRET_KEY = ''
+
+# for front-end testing with Sauce
+REMOTE_TESTING = {
+    'enabled': False,
+    'hub_url': 'http://%s:%s@ondemand.saucelabs.com:80/wd/hub',
+    'username': '',
+    'access_key': '',
+    'capabilities': {
+        # 'browser': 'internet explorer',
+        # 'version': '9.0',
+        # 'platform': 'Windows 7'
+    }
+}
