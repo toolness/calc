9c85a921fd9a167c86ce1fdfaa7cd34c8f6c9008
Maya Benari <hello@maya-benari.com>
2015-05-15 18:56:01 -0700

    Add svg inline and style w/ css

--- hourglass_site/static/hourglass_site/js/index.js
+++ hourglass_site/static/hourglass_site/js/index.js
@@ -525,7 +525,7 @@
       var id = d.string.split('-').join('');
       return '<a target="_blank" href="https://www.gsaadvantage.gov/ref_text/' 
              + id + '/' + id + '_online.htm">' + d.string
-             + '<img class="document-icon" src="static/hourglass_site/images/document.svg" alt="document icon"></a>';
+             + '<svg class="document-icon" width="8" height="8" viewBox="0 0 8 8"><path d="M0 0v8h7v-4h-4v-4h-3zm4 0v3h3l-3-3zm-3 2h1v1h-1v-1zm0 2h1v1h-1v-1zm0 2h4v1h-4v-1z" /></svg>';
     });
 
     // add a link to incoming exclusion cells
