041af8e0bcb05f08d4fb4080ce05d8c2558e7000
Theresa Summa <theresa.summa@gsa.gov>
2015-06-18 19:44:22 +0000

    adding migrations and data loading into CF script

---
+++ cf.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+echo "------ Starting APP ------"
+if [ $CF_INSTANCE_INDEX = "0" ]; then
+    echo "----- Migrating Database -----"
+    python manage.py migrate --noinput
+    echo "----- Loading Labor Category Data -----"
+    python manage.py load_data
+fi
+newrelic-admin run-program waitress-serve --port=$VCAP_APP_PORT hourglass.wsgi:application
