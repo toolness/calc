edd8181bd387f791cd10c5b72240875d669341ce
xtine <christine@fieryorange.com>
2015-06-19 11:18:11 -0700

    proposed price input

--- hourglass_site/static/hourglass_site/js/index.js
+++ hourglass_site/static/hourglass_site/js/index.js
@@ -542,7 +542,7 @@
     })
     .html(function(d) {
       var id = d.string.split('-').join('');
-      return '<a target="_blank" href="https://www.gsaadvantage.gov/ref_text/' 
+      return '<a target="_blank" href="https://www.gsaadvantage.gov/ref_text/'
              + id + '/' + id + '_online.htm">' + d.string
              + '<svg class="document-icon" width="8" height="8" viewBox="0 0 8 8"><path d="M0 0v8h7v-4h-4v-4h-3zm4 0v3h3l-3-3zm-3 2h1v1h-1v-1zm0 2h1v1h-1v-1zm0 2h4v1h-4v-1z" /></svg>';
     });
@@ -935,5 +935,26 @@
       });
     }
   }
+
+  function isNumberKey(evt){
+      var charCode = (evt.which) ? evt.which : event.keyCode
+      if (charCode > 31 && (charCode < 48 || charCode > 57))
+          return false;
+      return true;
+  }
+  // restrict proposed price input to be numeric only
+  $('.proposed-price input').keypress(function (e) {
+    if(!isNumberKey(e)) {
+      e.preventDefault();
+    }
+  })
+
+  $('.proposed-price button').click(function () {
+
+    $('.proposed-price-highlight').html('$' + $('.proposed-price input').val());
+    $('.proposed-price-block').fadeIn();
+
+  });
+
 
 })(this);
