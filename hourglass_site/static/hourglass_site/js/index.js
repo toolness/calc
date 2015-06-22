e289e06f2c2b4dbbbb586d8b4fd312f811e0aea4
xtine <christine@fieryorange.com>
2015-06-22 11:20:32 -0700

    education multi-select api integration

--- hourglass_site/static/hourglass_site/js/index.js
+++ hourglass_site/static/hourglass_site/js/index.js
@@ -536,7 +536,7 @@
     })
     .html(function(d) {
       var id = d.string.split('-').join('');
-      return '<a target="_blank" href="https://www.gsaadvantage.gov/ref_text/' 
+      return '<a target="_blank" href="https://www.gsaadvantage.gov/ref_text/'
              + id + '/' + id + '_online.htm">' + d.string
              + '<svg class="document-icon" width="8" height="8" viewBox="0 0 8 8"><path d="M0 0v8h7v-4h-4v-4h-3zm4 0v3h3l-3-3zm-3 2h1v1h-1v-1zm0 2h1v1h-1v-1zm0 2h4v1h-4v-1z" /></svg>';
     });
@@ -930,50 +930,52 @@
     }
   }
 
+
+  /*
+    Dropdown with Multiple checkbox select with jQuery - May 27, 2013
+    (c) 2013 @ElmahdiMahmoud
+    license: http://www.opensource.org/licenses/mit-license.php
+  */
+
+  $(".dropdown dt a").on('click', function (e) {
+      $(".dropdown dd ul").slideToggle('fast');
+
+      e.preventDefault();
+  });
+
+  $(".dropdown dd ul li a").on('click', function (e) {
+      $(".dropdown dd ul").hide();
+
+      e.preventDefault();
+  });
+
+  function getSelectedValue(id) {
+    return $("#" + id).find("dt a span.value").html();
+  }
+
+  $(document).bind('click', function (e) {
+    var $clicked = $(e.target);
+    if (!$clicked.parents().hasClass("dropdown")) $(".dropdown dd ul").hide();
+  });
+
+
+  $('.mutliSelect input[type="checkbox"]').on('click', function () {
+
+      var title = $(this).next().html();
+
+      if ($(this).is(':checked')) {
+        var html = '<span title="' + title + '">' + title + '</span>';
+
+        $('.multiSel').append(html);
+        $(".hide").hide();
+      }
+      else {
+        $('span[title="' + title + '"]').remove();
+        var ret = $(".hide");
+        $('.dropdown dt a').append(ret);
+      }
+  });
+
 })(this);
 
-/*
-  Dropdown with Multiple checkbox select with jQuery - May 27, 2013
-  (c) 2013 @ElmahdiMahmoud
-  license: http://www.opensource.org/licenses/mit-license.php
-*/ 
 
-      $(".dropdown dt a").on('click', function (e) {
-          $(".dropdown dd ul").slideToggle('fast');
-
-          e.preventDefault();
-      });
-
-      $(".dropdown dd ul li a").on('click', function (e) {
-          $(".dropdown dd ul").hide();
-
-          e.preventDefault();
-      });
-
-      function getSelectedValue(id) {
-           return $("#" + id).find("dt a span.value").html();
-      }
-
-      $(document).bind('click', function (e) {
-          var $clicked = $(e.target);
-          if (!$clicked.parents().hasClass("dropdown")) $(".dropdown dd ul").hide();
-      });
-
-
-      $('.mutliSelect input[type="checkbox"]').on('click', function () {
-        
-          var title = $(this).closest('.mutliSelect').find('input[type="checkbox"]').val(),
-              title = $(this).val() + ",";
-        
-          if ($(this).is(':checked')) {
-              var html = '<span title="' + title + '">' + title + '</span>';
-              $('.multiSel').append(html);
-              $(".hida").hide();
-          } 
-          else {
-              $('span[title="' + title + '"]').remove();
-              var ret = $(".hida");
-              $('.dropdown dt a').append(ret);
-              
-          }
-      });
