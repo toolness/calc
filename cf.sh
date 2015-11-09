6375a26ae964768fab5a56bfe521ce69ff9130f6
Theresa Summa <theresa.summa@gsa.gov>
2015-11-09 14:36:49 +0000

    loading s70 data on deploy

--- cf.sh
+++ cf.sh
@@ -5,5 +5,6 @@ if [ $CF_INSTANCE_INDEX = "0" ]; then
     python manage.py migrate --noinput
     echo "----- Loading Labor Category Data -----"
     python manage.py load_data
+    python manage.py load_s70
 fi
 newrelic-admin run-program waitress-serve --port=$VCAP_APP_PORT hourglass.wsgi:application
