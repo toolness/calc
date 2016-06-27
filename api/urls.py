49d719acf71db59e193fa38cdac5196e4feafda9
James Seppi <james.seppi@gmail.com>
2016-06-27 10:37:02 -0500

    use preferred url import syntax, fixes #318; pep8 (#323)

--- api/urls.py
+++ api/urls.py
@@ -1,12 +1,9 @@
-from django.conf.urls import patterns, include, url
-from rest_framework.urlpatterns import format_suffix_patterns
+from django.conf.urls import patterns, url
 from api import views
 
-
-urlpatterns = patterns('',
+urlpatterns = patterns(
+    '',
     url(r'^rates/$', views.GetRates.as_view()),
     url(r'^rates/csv/$', views.GetRatesCSV.as_view()),
     url(r'^search/$', views.GetAutocomplete.as_view()),
 )
-
-#urlpatterns = format_suffix_patterns(urlpatterns, allowed=['json', 'html', 'csv'])
--- hourglass/urls.py
+++ hourglass/urls.py
@@ -2,16 +2,17 @@ from django.conf.urls import patterns, include, url
 from django.contrib import admin
 from django.views.generic import TemplateView
 
-from api import urls as api_urls
 from .robots import robots_txt
 
-urlpatterns = patterns('',
+urlpatterns = patterns(
+    '',
     # Examples:
     # url(r'^$', 'hourglass.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
     url(r'^$', TemplateView.as_view(template_name='index.html'), name='index'),
-    url(r'^about/$', TemplateView.as_view(template_name='about.html'), name='about'),
-    url(r'^api/', include(api_urls)),
+    url(r'^about/$', TemplateView.as_view(template_name='about.html'),
+        name='about'),
+    url(r'^api/', include('api.urls')),
     url(r'^admin/', include(admin.site.urls)),
     url(r'^tests/$', TemplateView.as_view(template_name='tests.html')),
     url(r'^robots.txt$', robots_txt),
